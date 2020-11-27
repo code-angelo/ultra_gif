973e8c0d6856bfd6bb7a5a4710eade297da7b0e4
code-angelo <rosillo_dev@outlook.com>
2020-11-27 17:24:57 -0600

    stagered grid

--- lib/src/pages/search_page.dart
+++ lib/src/pages/search_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../colors.dart';
- 
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
  
 class SearchPage extends StatelessWidget {
   @override
@@ -27,11 +28,22 @@ class SearchPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(
-          
-        ),
-      ),
+      body: StaggeredGridView.countBuilder(
+        crossAxisCount: 4,
+        itemCount: 8,
+        itemBuilder: (BuildContext context, int index) => new Container(
+            color: PURPLE,
+            child: new Center(
+              child: new CircleAvatar(
+                backgroundColor: Colors.white,
+                child: new Text('$index'),
+              ),
+            )),
+        staggeredTileBuilder: (int index) =>
+            new StaggeredTile.count(2, index.isEven ? 2 : 1),
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+      )
     );
   }
 }
